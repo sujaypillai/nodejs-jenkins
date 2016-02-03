cb83c3084bb60515e64d925405aa39a35b08bc95
Sujay Pillai <sujayopillai@gmail.com>
2016-02-04 01:06:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http=require('http');
 
 var handleRequest = function(request,response){
 	response.writeHead(200);
-	response.end("!!!Hello World!!!");
+	response.end("!!!Beecon!!!");
 }
 
 var www = http.createServer(handleRequest);
