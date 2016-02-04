cca59fc770715935ff58b411256ed06cf0dfb721
Sujay Pillai <sujayopillai@gmail.com>
2016-02-04 14:40:31 +0800

    Update server.js
    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http=require('http');
 
 var handleRequest = function(request,response){
 	response.writeHead(200);
-	response.end("!!!Beecon!!!");
+	response.end("!!!Beecon Changes go here!!!");
 }
 
 var www = http.createServer(handleRequest);
